9cdab1a7ad9b89e9efb9abad51bcf0f6616c6e7e
LuketoDev <lucasbravouni@gmail.com>
2025-04-30 02:46:20 -0300

    \/ barra agregada antes del configRutas.php en los require

--- configRutas.php
+++ configRutas.php
@@ -1,6 +1,6 @@
 <?php
 
 // Ruta base del servidor (para includes y requires)
-define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/');
+define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . '/'); // usar /TP-Pokedex en la pc
 // URL base del sitio (para href, src, etc.)
-define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/');
+define('BASE_URL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/'); // usar /TP-Pokedex en la pc
